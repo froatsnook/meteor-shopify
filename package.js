a873b0b5bbeadfe2cb5641459bf1dcc1e3dc4713
froatsnook <froatsnook@gmail.com>
2015-12-17 08:53:49 +0100

    Bump version to 1.5.1.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: "froatsnook:shopify",
-    version: "1.5.0",
+    version: "1.5.1",
     summary: "Shopify API Access",
     git: "https://github.com/froatsnook/meteor-shopify",
     documentation: "README.md"
